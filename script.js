b660d85f5880c90a16500a8b949c3fd26da85aa1
Raj Aryan Dash <rajaryanrad@gmail.com>
2023-12-20 16:09:24 +0530

    Added remove and clear functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearBtn = document.getElementById('clear')
 
 function addItem(e) {
     e.preventDefault()
@@ -40,7 +41,21 @@ function createIcon(classes){
     return icon
 }
 
+function removeItem(e){
+    if (e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove()
+    }
+}
+
+function clearItems(e){
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 // add event listeners
 
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
 
