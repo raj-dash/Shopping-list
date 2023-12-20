8f016ade51b7530696e168e28456d863d0a90912
Raj Aryan Dash <rajaryanrad@gmail.com>
2023-12-20 16:20:55 +0530

    Added visual improvements to filter and clear btn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
 
 function addItem(e) {
     e.preventDefault()
@@ -21,7 +22,10 @@ function addItem(e) {
     const button = createButton("remove-item btn-link text-red")
     li.appendChild(button)
 
+    // add li to the dom
     itemList.appendChild(li)
+
+    checkUI()
 
     itemInput.value = ''
 }
@@ -43,13 +47,30 @@ function createIcon(classes){
 
 function removeItem(e){
     if (e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove()
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove()
+            checkUI()
+        }
     }
 }
 
 function clearItems(e){
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild)
+    }
+
+    checkUI()
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li')
+
+    if (items.length === 0){
+        clearBtn.style.display = 'none'
+        itemFilter.style.display = 'none'
+    } else {
+        clearBtn.style.display = 'block'
+        itemFilter.style.display = 'block'
     }
 }
 
@@ -59,3 +80,4 @@ itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
 
+checkUI()
